11ec64328b9037619691cdc60003107fd945fa6d
pechenka366 <114060538+pechenka366@users.noreply.github.com>
2022-11-01 19:21:22 +0900

    jq
    jq

--- src/js/num.js
+++ src/js/num.js
@@ -1,83 +1,27 @@
-
-/*
-var time = 2,
-  cc = 1;
-$(window).scroll(function() {
-  $('#counter').each(function() {
-    var
-      cPos = $(this).offset().top,
-      topWindow = $(window).scrollTop();
-    if (cPos < topWindow + 200) {
-      if (cc < 2) {
-        $(".number").addClass("viz");
-        $('div').each(function() {
-          var
-            i = 1,
-            num = $(this).data('num'),
-            step = 1000 * time / num,
-            that = $(this),
-            int = setInterval(function() {
-              if (i <= num) {
-                that.html(i);
-              } else {
-                cc = cc + 2;
-                clearInterval(int);
-              }
-              i++;
-            }, step);
-        });
-      }
-    }
-  });
+$(document).ready(function () {
+ 
+    var show = true;
+    var countbox = ".benefits__inner";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false;
+        var w_top = $(window).scrollTop(); 
+        var e_top = $(countbox).offset().top; 
+        var w_height = $(window).height(); 
+        var d_height = $(document).height(); 
+        var e_height = $(countbox).outerHeight(); 
+        if (w_top + 1000 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.benefits__number').css('opacity', '1');
+            $('.benefits__number').spincrement({
+                thousandSeparator: "",
+                duration: 3500
+            });
+             
+            show = false;
+        }
+    });
+ 
 });
 
 
-
-
-
-$(document).ready(function(){
-
-    let time = 2;
-    $("stats_item").each(function(){
-        $("div").each(function(){
-            let i = 1,
-                numsc = $(this).data("numsc"),
-                step = 1000 * time / numsc,
-                that = $(this),
-                int = setInterval(function(){
-                    if (i <= numsc){
-                        that.html(i);
-                    }
-                    else{
-                        clearInterval(int);
-                    }
-                    i++
-                }, step);
-        });
-    });
-    
-};
-
-
-
-
-/*
-$(document).ready(function(){
-    
-const time = 3500;
-const step = 100;
-
-function number(num, elem){
-    let j = document.querySelector('.stats_item .stats_num' + elem);
-    l = 0;
-    let m = Math.round(time/(number/step))
-    let inter = setInterval(() => {
-        l += max;
-        if (l == number){
-            clearInterval(inter);
-        }
-    })
-};
-
-};
-*/
+                  
+                  
